f782bc02d4ad5f1c9d8360d9bc9c7ef967ef78c8
Murat <107918661+1MuratBasar@users.noreply.github.com>
2023-12-12 21:37:29 +0300

    Aritmetik İşlemler ve İşlem Önceliği

---
+++ Hafta-1/Aritmetik Islemler ve Islem Onceligi/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // ihtiyac duyacagımız değişkenleri tanımlıyoruz
+        int a,b,c,result;
+        /*dışarıdan veri almak için scanner nesnemizi tanımlıyor, kullanıcıdan veri istiyor ve
+        bunu bir degişkenlerimize atıyoruz.*/
+        Scanner input = new Scanner(System.in);
+        System.out.print("a sayısını giriniz : ");
+        a = input.nextInt();
+        System.out.print("b sayısını giriniz : ");
+        b = input.nextInt();
+        System.out.print("c sayısını giriniz : ");
+        c = input.nextInt();
+        // işlem adımlarına ayırıp, sonuc hesaplatıp bunu yazdırıyoruz.
+        result = (a+(b*c)-b);
+        System.out.println("Sonuc : " + result);
+    }
+
+}
