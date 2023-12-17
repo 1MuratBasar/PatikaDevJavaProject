3fd3fd0bcb0e16ed957bf9c4710b6ad64a31358f
Murat <107918661+1MuratBasar@users.noreply.github.com>
2023-12-18 02:48:53 +0300

    Faktoriyel Hesaplama

---
+++ Hafta-1/Faktoriyel Hesaplayan Program/src/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Faktoriyeli Hesaplanacak Değeri Girin : ");
+        int n = input.nextInt();
+        if (n <=0) {
+            System.out.println("0 veya daha az değer girdiniz, tekrar deneyin.");
+        } else {
+
+
+            int total = 1;
+
+            for (int i = 1; i <= n; i++) {
+
+                total = total * i;
+            }
+            System.out.println("Girilen sayının faktoriyeli : " + total);
+        }
+    }
+}
