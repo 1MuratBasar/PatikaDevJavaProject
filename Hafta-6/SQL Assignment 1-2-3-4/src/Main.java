a5c05743dbf365a818278ba23fa5a5046ca4b103
Murat <107918661+1MuratBasar@users.noreply.github.com>
2024-01-18 02:55:22 +0300

    SQL 1-2-3-4 Ödevleri

---
+++ Hafta-6/SQL Assignment 1-2-3-4/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        System.out.println("Merhaba !");
+        System.out.println("Patika 6. Hafta SQL-1-2-3-4 Ödevi Readme dosyalarındadır  !");
+
+    }
+}
