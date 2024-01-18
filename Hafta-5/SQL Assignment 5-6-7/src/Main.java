d00a1eb877ca23bd3911cefceff768348415b374
Murat <107918661+1MuratBasar@users.noreply.github.com>
2024-01-18 11:10:57 +0300

    SQL ödev 5-6-7

---
+++ Hafta-5/SQL Assignment 5-6-7/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Merhaba !");
+        System.out.println("SQL ödevleri README dosyalarındadır...");
+
+
+    }
+}
