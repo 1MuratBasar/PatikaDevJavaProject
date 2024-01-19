5d94966acc08d80ab2c0b689b60c31307ae8c27a
Murat <107918661+1MuratBasar@users.noreply.github.com>
2024-01-19 03:46:45 +0300

    SQL Ödev 9-10-11

---
+++ Hafta-6/SQL Assignment 9-10-11/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Merhaba !");
+        System.out.println("SQL 9-10-11 Ödev README dosyalarındadır.");
+    }
+}
