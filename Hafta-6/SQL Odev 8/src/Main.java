5503e5e853313d9354a809652410f74c40d43409
Murat <107918661+1MuratBasar@users.noreply.github.com>
2024-01-19 03:26:19 +0300

    SQL 8 assignment

--- Hafta-6/SQL Odev 8/src/Main.java
+++ Hafta-6/SQL Odev 8/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Merhaba !");
-        System.out.println("SQL8 Ödev README.MD dosyasındadır.");
+        System.out.println("SQL8 Ödev README dosyasındadır.");
     }
 }
