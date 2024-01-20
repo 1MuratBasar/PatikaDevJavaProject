e8f82e9397e1f3fa70eb79f72d9e02780b699da4
Murat <107918661+1MuratBasar@users.noreply.github.com>
2024-01-20 22:12:58 +0300

    SQL 12 Assignment

---
+++ Hafta-6/SQL Assignment 12/src/Main.java
@@ -0,0 +1,7 @@
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Merhaba!");
+        System.out.println("SQL12 Ödev README.MD dosyasındadır !");
+    }
+}
