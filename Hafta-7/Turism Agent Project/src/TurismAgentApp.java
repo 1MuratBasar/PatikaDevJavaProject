7e30fe581ac2ed8a267a7fe6d6234a6296a68e8f
Murat <107918661+1MuratBasar@users.noreply.github.com>
2024-02-10 23:56:52 +0300

    Revize

--- Hafta-7/Turism Agent Project/src/TurismAgentApp.java
+++ Hafta-7/Turism Agent Project/src/TurismAgentApp.java
@@ -6,7 +6,9 @@ import view.LoginView;
 public class TurismAgentApp {
     public static void main(String[] args) {
 
-        EmployeeView employeeView = new EmployeeView(new User());
-      //  LoginView loginView = new LoginView();
+      //  EmployeeView employeeView = new EmployeeView(new User());
+        LoginView loginView = new LoginView();
+        //kullanıcı admin şifre 1234
+        //kullanıcı emp şifre 5678
     }
 }
